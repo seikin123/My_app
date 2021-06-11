4d9705c1383eabcf87566e083edc924925fe435e
ヨシダタイキ <abc521fjr7@gmail.com>
2021-06-11 14:49:13 +0900

    [add]users_resources追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :question
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :questions
+  
 end
